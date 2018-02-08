0c1983711fad1554e5e0e42fe3b1fc62b4e15d66
Elle Wilson <eleanorwilsonn@gmail.com>
2018-02-08 16:58:38 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   if array.length <= 2
     array.join (" and ")
   else array.insert(-2, "and ")
-    array[0...-1].join(", ")
+    array[0...-1].join(", ") << array[-1]
   end
-  oxford_comma
+
 end
