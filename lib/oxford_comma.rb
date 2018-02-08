ba3edc2ff94b8a531a0a30122a9cd49f86e6b786
Elle Wilson <eleanorwilsonn@gmail.com>
2018-02-08 16:43:39 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,6 +3,6 @@
 def oxford_comma(array)
   if array.length <= 2
     array.join (" and ")
-  else array[0...-1].join(", ") << ", and "
+  else array[0...-1].join(", ") && array[-1] << ", and "
   end
 end
