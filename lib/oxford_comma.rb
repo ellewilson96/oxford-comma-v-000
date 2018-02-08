375e5507383279e31428229a8a3d719e2ab57f7b
Elle Wilson <eleanorwilsonn@gmail.com>
2018-02-08 16:33:18 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,8 @@ new_array = ["fiddleheads","okra","kohlrabi"]
 
 
 def oxford_comma(array)
-  if array.length == 2, array[1].join(" and ")
+  if array.length == 2
+     array[1].join(" and ")
   else array.join(", ")
 end
 end
